64d489d508e2aedb98fffc98e70a433e33b4e60a
Mancol001 <166870525+Mancol001@users.noreply.github.com>
2024-04-18 14:16:21 -0400

    Add files via upload

---
+++ deadPC.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Your pc will died in (FLASH WARRING)
+timeout 6
+
+:ha
+color 47
+start https://cdn.vectorstock.com/i/1000v/83/41/dead-computer-vector-988341.jpg
+color 07
+goto ha
